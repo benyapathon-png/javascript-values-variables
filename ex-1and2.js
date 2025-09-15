9a64b33142fe3ed3ee74db03ba68ea1cd9e8557a
Ben chan <benyapa.thon@gmail.com>
2025-09-15 22:19:10 +0700

    do ex1and2.js

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter"
+let lastName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+console.log(firstName)
+console.log(lastName)
+console.log(address)    
+const resterantName = "Chef's Table by Chef James"
+const totalPrice = 15000
+const totalResterant = 1500
+console.log(resterantName)
+console.log(totalPrice)
+console.log(totalResterant)
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+console.log(address);
