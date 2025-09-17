3122a67bd573736cf43522ed024113a77580a7f8
Ben chan <benyapa.thon@gmail.com>
2025-09-17 18:23:58 +0700

    do ex-3

--- ex-3.js
+++ ex-3.js
@@ -1,7 +1,5 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
 // Start coding here;
-console.log(celsius * 1.8) + 32
-let fahrenheit;
-fahrenheit = (celsius * 1.8) + 32;
-console.log(fahrenheit);
+let fahrenheit = (celsius * 1.8) + 32;
+console.log(fahrenheit); 
